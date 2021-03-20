0fb337f38b01ca8a8e3e3b98479274b76d5335aa
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2021-03-20 11:07:02 -0400

    add bonding curve text

--- src/components/BuySell.js
+++ src/components/BuySell.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class BuySell extends Component {
     render() { 
         return <div className="flex flex-row justify-between space-x-4">
+            <p>The bonding curve is...</p>
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex">Buy</button>  
             <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex">Sell</button>    
         </div>
