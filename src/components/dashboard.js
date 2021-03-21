4ea47f412fff9626c8ac5d3ae751c7a8872bcd72
jennifertrin <38402540+jennifertrin@users.noreply.github.com>
2021-03-21 00:23:43 -0400

    updated to one endpoint

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -3,33 +3,30 @@ import axios from 'axios'
 import {useParams} from 'react-router-dom'
 
 function DataFetching() {
-    const [contract, setContract] = useState(null);
-    const [transactions, setTransactions] = useState(null);
-
-    const fetchData = () => {
     const url = 'https://api.covalenthq.com/v1/1/tokens/0xb80fbf6cdb49c33dc6ae4ca11af8ac47b0b4c0f3/nft_metadata/432/?key=ckey_693c563ac29c41bcbccfbe9326d';
-    const url2 = 'https://api.covalenthq.com/v1/1/tokens/0xb80fbf6cdb49c33dc6ae4ca11af8ac47b0b4c0f3/nft_transactions/432/?key=ckey_693c563ac29c41bcbccfbe9326d';
-    
-    const getMetaData = axios.get(url);
-    const getTransactions = axios.get(url2);
-    
-    axios.all([getMetaData, getTransactions]).then(
-        axios.spread((...allData) => {
-            const allDataPlayer = allData;
-            const getTranData = allData;
+    const [contract, setContract] = useState(null);
 
-            console.log(allDataPlayer);
-            console.log(getTranData);
-        })
-    )
-    }
+    let content = null
 
     useEffect(() => {
-        fetchData()
-    }, [])
+        axios.get(url) 
+        .then(response => {
+            setContract(response.data)
+        })
+    }, [url])
+
+    if(contract) {
+        content = <div></div>
+    }
+
+    console.log(contract)
 
     return (
-        <div>  
+        <div>
+        <h1 className="text-2xl">Contract Metadata</h1>
+        <p className="text-lg font-sans font-medium">Contract address: {contract.data.items[0].contract_address}</p>
+        <p className="text-lg font-sans font-medium">Contract address: {contract.data.items[0].contract_name}</p>
+        <p className="text-lg font-sans font-medium">Contract address: {contract.data.items[0].contract_name}</p>
         </div>
     )
   }
